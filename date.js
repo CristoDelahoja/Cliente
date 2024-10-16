2ca6c6fed60c43997f721c48d43a4bf128dc7837
CristoDelahoja <cristo.mates@gmail.com>
2024-10-16 19:46:02 +0200

    16/10

--- date.js
+++ date.js
@@ -5,7 +5,15 @@ class SaludoUsuario {
     }
 
     mostrarSaludo() {
-        const opciones = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
+        const opciones = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false
+        };
         const fechaHoraFormateada = this.fechaHoraActual.toLocaleString('es-ES', opciones);
         console.log(`¡Hola, ${this.nombreUsuario}! La fecha y hora actuales son: ${fechaHoraFormateada}`);
     }
